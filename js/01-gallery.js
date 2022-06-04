56fe37ae8e06ffc3128a3837a8b3a62392ad5ac2
Vitalii-Seniura <senyura.w@gmail.com>
2022-06-05 00:46:51 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,7 +10,7 @@ const galleryItem = galleryItems.map((
     `<div class='gallery__item'>
     <a class="gallery__link" href='${original}'>
     <img class='gallery__image' src='${preview}' 
-    data-source='${original}' alt='${description}'/>
+    data-source='${original} alt='${description}'/>
     </a></div>`).join("");
 
 gallery.insertAdjacentHTML("beforeend", galleryItem);
